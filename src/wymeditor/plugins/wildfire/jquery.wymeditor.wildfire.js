625dce48ee077603944ff88165074a6c8f8acb59
Ross <ross@Rosss-MacBook-Pro-2.local>
2009-04-18 13:31:39 +0100

    Work on inline image editor for wysiwyg

--- src/wymeditor/plugins/wildfire/jquery.wymeditor.wildfire.js
+++ src/wymeditor/plugins/wildfire/jquery.wymeditor.wildfire.js
@@ -4,7 +4,7 @@
  *    1. Link interceptor to give a choice between regular urls and file urls
  *    2. Insert Video Button
  *    3. Insert Audio Button
- *  
+ *    4. Overwrite default image insert to be awesome
  */
 
 //Extend WYMeditor
@@ -13,6 +13,11 @@ WYMeditor.editor.prototype.wildfire = function(options) {
   $(".wym_tools_subscript").remove();
   $(".wym_tools_preview").remove();
   $(".wym_classes").removeClass("wym_panel").addClass("wym_dropdown");
+  
+  /*******************************************/
+  /* Overwrite default link insert */
+  /*******************************************/
+  
   $(".wym_tools_link a").unbind("click");
   $(".wym_tools_link a").click(function(){
     $("#link_dialog").dialog("open");
@@ -33,7 +38,10 @@ WYMeditor.editor.prototype.wildfire = function(options) {
     return false;
   });
   
+  /*******************************************/
   /* Video Insertion Button */
+  /*******************************************/
+  
   var vidhtml = wym_button("video", "Insert a Video");
   $(wym._box).find(".wym_tools_image").after(vidhtml);
   $(".wym_tools_video a").click(function(){
@@ -48,13 +56,26 @@ WYMeditor.editor.prototype.wildfire = function(options) {
       $("#video_dialog").dialog("close");
     });
   });
+  
+  /*******************************************/
   /* Audio Insertion Button */
+  /*******************************************/
+  
   var audhtml = wym_button("audio", "Embed an Audio File");
   $(wym._box).find(".wym_tools_video").after(audhtml);
   $(".wym_tools_audio a").click(function(){
     var audiofile = prompt("Enter Audio Filename");
     if(audiofile) wym._exec("inserthtml","<a href='#' rel='audiofile'>"+audiofile+"</a>");
   });
+  
+  /*******************************************/
+  /* Inline Image Insertion Button */
+  /*******************************************/
+  $(".wym_tools_image a").unbind("click");
+  $(".wym_tools_image a").click(function(){
+    show_inline_image_browser();
+  });
+  
   
 };
 
@@ -65,3 +86,105 @@ function wym_button(name, title) {
               + "</a></li>";
   return html;
 }
+
+
+function initialise_inline_image_edit() {
+  theIframe = $("#cms_content_contentWidgIframe").get(0);
+  $("#cms_content_contentWidgIframe").contents().find(".inline_image").dblclick(function(){
+    image_to_edit = $(this);
+    var image_browser = '<div id="inline_image_browser" class="inline_edit_existing"><div id="inline_close_bar"><h3>Edit Image</h3><a id="inline_close" href="#">x</a></div></div>';
+    $("body").append(image_browser);
+    $("#inline_image_browser").centerScreen();
+    $("#inline_close").click(function(){
+      $("#inline_image_browser").remove(); return false;
+    });
+    $.get("/admin/files/inline_image_edit", function(response){
+      $("#inline_image_browser").append(response);
+      $("#selected_image img").attr("src", image_to_edit.attr("src")).css("width", "90px");
+      $("#image_meta input").removeAttr("disabled");
+      $("#meta_description").val(image_to_edit.attr("alt"));
+      if(image_to_edit.hasClass("flow_left")) $("#flow_left input").attr("checked", true);
+      if(image_to_edit.hasClass("flow_right")) $("#flow_right input").attr("checked", true);
+      if(image_to_edit.parent().is("a")) $("#inline_image_link").val(image_to_edit.parent().attr("href"));
+      $("#inline_insert .generic_button a").click(function(){
+        if($("#flow_normal input").attr("checked")) var img_class = "inline_image flow_normal";
+        if($("#flow_left input").attr("checked")) var img_class = "inline_image flow_left";
+        if($("#flow_right input").attr("checked")) var img_class = "inline_image flow_right";
+        var img_html= '<img style="" src="'+$("#selected_image img").attr("src")+'" class="'+img_class+'" alt="'+$("#meta_description").val()+'" />';
+        if($("#inline_image_link").val().length > 1) img_html = '<a href="'+$("#inline_image_link").val()+'">'+img_html+"</a>";
+        theIframe.contentWindow.document.execCommand("inserthtml", false, img_html);
+    		$("#inline_image_browser").remove(); return false;
+    		initialise_inline_image_edit();
+      });
+    });
+  });
+}
+
+
+
+var inline_image_filter_timer;
+
+function inline_image_filter_post(){
+  $.post("/admin/files/image_filter",
+    {filter: $("#filter_field").val()}, 
+    function(response){ 
+      $("#inline_image_browser #image_display").html(response);
+      init_inline_image_select();
+      clearTimeout(inline_image_filter_timer);
+    }
+  );
+}
+
+function show_inline_image_browser() {
+  var image_browser = '<div id="inline_image_browser"><div id="inline_close_bar"><h3>Insert Image</h3><a id="inline_close" href="#">x</a></div></div>';
+  $("body").append(image_browser);
+  $("#inline_image_browser").centerScreen();
+  $("#inline_close").click(function(){
+    $("#inline_image_browser").remove(); return false;
+  });
+  $.get("/admin/files/inline_browse/1/", function(response){
+    $("#inline_image_browser").append(response);
+    init_inline_image_select();
+    
+    $("#inline_image_browser #filter_field").keyup(function(e) {
+			if (e.which == 8 || e.which == 32 || (65 <= e.which && e.which <= 65 + 25) || (97 <= e.which && e.which <= 97 + 25) || e.which == 160 || e.which == 127) {
+				clearTimeout(inline_image_filter_timer);
+				inline_image_filter_timer = setTimeout("inline_image_filter_post()", 800);
+			}
+    });
+  });
+}
+
+function init_inline_image_select() {  
+  $("#image_display .edit_img").remove();
+  $("#image_display div img").hover(function(){$(this).css("border", "2px solid #222");}, function(){ $(this).css("border","2px solid white");} );
+  $("#image_display div img").click(function(){
+    $("#image_meta input").removeAttr("disabled");
+    $("#selected_image img").attr("src", "/show_image/"+$(this).parent().attr("id")+"/90.jpg");
+    $("#inline_insert .generic_button a").click(function(){
+      if($("#flow_normal input").attr("checked")) var img_class = "inline_image flow_normal";
+      if($("#flow_left input").attr("checked")) var img_class = "inline_image flow_left";
+      if($("#flow_right input").attr("checked")) var img_class = "inline_image flow_right";
+      var img_html= '<img style="" src="'+$("#selected_image img").attr("src")+'" class="'+img_class+'" alt="'+$("#meta_description").val()+'" />';
+      if($("#inline_image_link").val().length > 1) img_html = '<a href="'+$("#inline_image_link").val()+'">'+img_html+"</a>";
+      theIframe.contentWindow.document.execCommand("inserthtml", false, img_html);
+  		$("#inline_image_browser").remove(); 
+  		initialise_inline_image_edit();
+  		return false;
+    });
+  });
+}
+
+jQuery.fn.centerScreen = function(loaded) { 
+  var obj = this; 
+  if(!loaded) { 
+    obj.css('top', $(window).height()/2-this.height()/2); 
+    obj.css('left', $(window).width()/2-this.width()/2); 
+    $(window).resize(function() { obj.centerScreen(!loaded); }); 
+  } else { 
+    obj.stop(); 
+    obj.animate({ 
+      top: $(window).height()/2-this.height()/2, 
+      left: $(window).width()/2-this.width()/2}, 200, 'linear'); 
+  } 
+};
