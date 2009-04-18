aa50142d11a71f13c821da8f6ba030bc268c77d3
Ross <ross@Rosss-MacBook-Pro-2.local>
2009-04-18 21:22:13 +0100

    Work on inline image editor for wysiwyg

--- src/wymeditor/plugins/wildfire/jquery.wymeditor.wildfire.js
+++ src/wymeditor/plugins/wildfire/jquery.wymeditor.wildfire.js
@@ -89,6 +89,7 @@ function wym_button(name, title) {
 
 
 function initialise_inline_image_edit(wym) {
+  wym.find(".inline_image").css("border", "1px solid red");
   wym.find(".inline_image").dblclick(function(){
     image_to_edit = $(this);
     var image_browser = '<div id="inline_image_browser" class="inline_edit_existing"><div id="inline_close_bar"><h3>Edit Image</h3><a id="inline_close" href="#">x</a></div></div>';
