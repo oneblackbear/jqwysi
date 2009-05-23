c5214d2b23821eb7ff80872728fbe82784ae9ec5
Ross <ross@Rosss-MacBook-Pro-2.local>
2009-05-23 12:07:10 +0100

    Fix to embed tag parsing

--- build/jquery.wymeditor.js
+++ build/jquery.wymeditor.js
@@ -3380,7 +3380,7 @@ WYMeditor.XhtmlSaxListener = function()
     this.block_tags = ["a", "abbr", "acronym", "address", "area", "b",
     "base", "bdo", "big", "blockquote", "body", "button",
     "caption", "cite", "code", "col", "colgroup", "dd", "del", "div",
-    "dfn", "dl", "dt", "em","embed", "fieldset", "form", "head", "h1", "h2",
+    "dfn", "dl", "dt", "em", "fieldset", "form", "head", "h1", "h2",
     "h3", "h4", "h5", "h6", "html", "i", "ins",
     "kbd", "label", "legend", "li", "map", "noscript",
     "object", "ol", "optgroup", "option", "p", "param", "pre", "q",
@@ -3389,7 +3389,7 @@ WYMeditor.XhtmlSaxListener = function()
     "thead", "title", "tr", "tt", "ul", "var", "extends"];
 
 
-    this.inline_tags = ["br", "hr", "img", "input"];
+    this.inline_tags = ["br", "hr", "img", "input", "embed"];
 
     return this;
 };
